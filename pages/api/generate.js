60c8431701635c6ecd02fc9569e29c49484259f8
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2023-10-20 19:57:53 +0100

    ServeTest

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -51,12 +51,7 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return `Play chess, act as both players and get to a stalemate, use the input as your chess names
+Input: ${capitalizedAnimal}
+`;
 }
